e68b8e97072c49fe1dc59ff0c7398aba75d108d6
Dominik <ja-do@wp.pl>
2020-12-11 11:08:24 +0100

    kalkulator

---
+++ main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+int main() {
+    float l1,l2,wynik;
+    char znak;
+
+    printf("Wpisz dzialanie(+,-,*,/): ");
+
+    scanf("%f %c %f",&l1,&znak,&l2);
+
+    switch(znak)
+    {
+
+        case '+':
+        {
+
+            wynik=l1+l2;
+
+            break;
+        }
+
+        case '-':
+        {
+
+            wynik=l1-l2;
+
+            break;
+        }
+
+        case '*':
+        {
+
+            wynik=l1*l2;
+
+            break;
+        }
+
+        case '/':
+        {
+
+            wynik=l1/l2;
+
+            break;
+        }
+
+        default:
+        {
+
+            printf("zle dzialanie");
+
+            break;
+        }
+    }
+    printf("Wynik dzialania: %.1f",wynik);
+
+
+
+    return 0;
+}
