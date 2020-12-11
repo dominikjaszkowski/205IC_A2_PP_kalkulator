b6dedd6ba8f29a9699b28fe57482d7a8bcc3eb1f
Dominik <ja-do@wp.pl>
2020-12-11 11:57:09 +0100

    kalkulator

--- main.c
+++ main.c
@@ -1,55 +1,54 @@
 #include <stdio.h>
 
 int main() {
-    float l1,l2,wynik;
+    int l1, l2;
+    float wynik;
     char znak;
 
     printf("Wpisz dzialanie(+,-,*,/): ");
 
-    scanf("%f %c %f",&l1,&znak,&l2);
+    scanf("%d %c %d", &l1, &znak, &l2);
 
-    switch(znak) {
-        case '+':
-            {
+    switch (znak) {
+        case '+': {
 
             wynik = l1 + l2;
 
             break;
         }
 
-        case '-':
-            {
+        case '-': {
 
             wynik = l1 - l2;
 
             break;
         }
 
-        case '*':
-            {
+        case '*': {
 
             wynik = l1 * l2;
 
             break;
         }
 
-        case '/':
-            {
-
-            wynik = l1 / l2;
-
-            break;
+        case '/': {
+            if (l1 == 0 || l2 == 0) {
+                wynik = 0;
+                break;
+            } else if (l1 != 0 || l2 != 0) {
+                wynik = (float) l1 / l2;
+                break;
+            }
         }
 
-        default:
-            {
+        default: {
 
             printf("zle dzialanie");
 
             return 0;
         }
     }
-    printf("Wynik dzialania: %.1f",wynik);
+    printf("Wynik dzialania: %.1f", wynik);
 
     return 0;
 }
